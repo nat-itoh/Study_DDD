a58cb042e637fc54aedb680b8c1f0714bdbab34a
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2024-11-29 00:03:17 +0900

    各種更新．

---
+++ Assets/Project/Scripts/Composition/ApplicationRoot.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using BmiApp.Domain.BmiCalculater.Repository;
+using BmiApp.Domain.BmiCalculater.Eintities;
+using BmiApp.Infrastructure.BmiCalculator;
+
+namespace BmiApp.Composition {
+
+
+    public class ApplicationRoot : MonoBehaviour {
+
+    }
+
+}
