be71358c4ac40127d8e0c3e2a7095e3e37acb523
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2024-12-24 00:25:09 +0900

    SoundSettingsの整備．

--- Assets/Project/Scripts/Domain/Setting/Model/SettingsModel.cs
+++ Assets/Project/Scripts/Domain/Setting/Model/SettingsModel.cs
@@ -1,9 +1,44 @@
-using UnityEngine;
+using System;
+using UniRx;
 
-namespace Project.Domain
-{
-    public class SettingsModel : MonoBehaviour
-    {
-    
+namespace Project.Domain.Setting.Model {
+
+    public sealed class SettingsModel : IDisposable {
+
+        private readonly ReactiveProperty<SoundSettingsSet> _soundSettingsSetRP;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IReadOnlyReactiveProperty<SoundSettingsSet> SoundSettingsSetRP => _soundSettingsSetRP;
+
+
+        /// ----------------------------------------------------------------------------
+        // Public Method
+
+        /// <summary>
+        /// コンストラクタ．
+        /// </summary>
+        public SettingsModel(SoundSettingsSet initialSettings) {
+            _soundSettingsSetRP = new ReactiveProperty<SoundSettingsSet>(initialSettings);
+        }
+                
+        /// <summary>
+        /// 終了処理．
+        /// </summary>
+        public void Dispose() {
+
+        }
+
+
+        /// ----------------------------------------------------------------------------
+        // Internal Method
+
+        /// <summary>
+        /// サウンド設定を更新．
+        /// </summary>
+        internal void UpdateSoundSettings(SoundSettingsSet newSettings) {
+            _soundSettingsSetRP.Value = newSettings;
+        }
     }
 }
