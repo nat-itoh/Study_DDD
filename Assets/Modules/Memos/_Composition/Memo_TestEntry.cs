769edbe16fdf5296277a87301e1e4c34b3073bb6
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2025-01-10 01:01:14 +0900

    各種更新．

--- Assets/Modules/Memos/_Composition/Memo_TestEntry.cs
+++ Assets/Modules/Memos/_Composition/Memo_TestEntry.cs
@@ -1,19 +1,55 @@
+using System;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using SQLite;
+using System.IO;
 
-namespace Project.Composition
-{
-    public class Memo_TestEntry : MonoBehaviour
-    {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
+namespace Project.Composition {
+
+    public class Memo_TestEntry : MonoBehaviour {
+
+        private void Start() {
+            var context = new DbContext();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
+    }
+
+
+    public class DbContext : IDisposable {
+
+        private readonly SQLiteConnection _connection;
+
+        public DbContext() {
+            var dbPath = Path.Combine(UnityEngine.Application.persistentDataPath, "memory.db");
+            Debug.Log(dbPath);
+
+            // SQLiteデータベースに接続
+            _connection = new SQLiteConnection(dbPath);
+
+            // テーブル作成
+            _connection.CreateTable<User>();
+
+            // データを挿入
+            _connection.Insert(new User { Name = "John Doe", Age = 30 });
+
+            // データを取得
+            var users = _connection.Table<User>();
+            foreach (var user in users) {
+                Debug.Log($"User: {user.Name}, Age: {user.Age}");
+            }
+        }
         
+        
+        public void Dispose() {
         }
     }
+
+    public class User {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+
 }
